f8b7e94a7e21aa673b4358624dbb0f14b20bc285
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2024-10-24 16:03:46 +0200

    UI Update

--- taskManager/src/modules/newTaskModule.ts
+++ taskManager/src/modules/newTaskModule.ts
@@ -4,14 +4,16 @@ export interface task {
     taskId: number,
     taskReward: number,
     taskCategory: string,
+    taskDate: Date,
 }
 
-export const createTask = (name: string, Id: number, reward: number, category: string) => {
+export const createTask = (name: string, Id: number, reward: number, category: string, date: Date) => {
     const task: task = {
         taskName: name,
         taskId: Id,
         taskReward: reward,
         taskCategory: category,
+        taskDate: date,
     }
     return task;
 }
--- taskManager/src/modules/taskManagerModule.ts
+++ taskManager/src/modules/taskManagerModule.ts
@@ -1,2 +1,8 @@
 import { createTask } from "./newTaskModule";
 import { taskItemsList } from "./taskItemsListModule";
+
+export const taskManagerContainer = document.querySelector('.task-manager-container') as HTMLDivElement;
+
+const renderTasks = (): void => {
+    
+}
