30ccd7359abb29711c12d0668786424f661f7b93
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2024-10-24 20:47:46 +0200

    Fixed button logic

--- taskManager/src/main.ts
+++ taskManager/src/main.ts
@@ -1,3 +1,11 @@
+import { initNewTaskButton, initSaveButton } from "./modules/taskManagerModule";
 
 
+const newTaskButton = document.getElementById('new-task-button') as HTMLButtonElement;
+const saveTaskButton = document.getElementById('save-task-button') as HTMLButtonElement;
 
+
+//Starting point
+saveTaskButton.style.display = 'none';
+initNewTaskButton(newTaskButton);
+initSaveButton(saveTaskButton);
--- taskManager/src/modules/taskManagerModule.ts
+++ taskManager/src/modules/taskManagerModule.ts
@@ -1,8 +1,63 @@
 import { createTask } from "./newTaskModule";
 import { taskItemsList } from "./taskItemsListModule";
 
-export const taskManagerContainer = document.querySelector('.task-manager-container') as HTMLDivElement;
 
 const renderTasks = (): void => {
+
+}
+
+
+
+export const initNewTaskButton = (buttonElement:HTMLButtonElement) => {
+
+    const taskList = document.querySelector('.task-list') as HTMLDivElement;
+    const saveTaskButton = document.getElementById('save-task-button') as HTMLButtonElement;
     
+    if (!taskList || !saveTaskButton ) {
+        throw new Error('taskList or SaveTaskButton not saved in the DOM');
+    }
+
+    //Creates new task input
+    if (buttonElement) {
+        buttonElement.addEventListener('click', () => {
+            console.log('Button clicked');
+        
+            //Hides newtask button and displays savetask button
+            buttonElement.style.display = 'none';
+            saveTaskButton.style.display = 'block';
+            
+            //Creates and appends task input container
+            const taskInputContainer: HTMLDivElement = document.createElement('div');
+            taskInputContainer.classList.add('task-input-container');
+            taskList.appendChild(taskInputContainer);
+            
+            //Creates task input form
+            const taskInputForm: HTMLInputElement = document.createElement('input');
+            taskInputForm.classList.add('task-input-form');
+            taskInputForm.id = 'task-input-form';
+            taskInputForm.type = 'text';
+            taskInputContainer.appendChild(taskInputForm);
+        });
+    }
+    else {
+        throw new Error('buttonElement not found in the DOM');
+    }
+};
+
+export const initSaveButton = (buttonElement:HTMLButtonElement) => {
+        const taskList = document.querySelector('.task-list') as HTMLDivElement;
+        const newTaskButton = document.getElementById('new-task-button');
+
+        if (!taskList || !newTaskButton) {
+            throw new Error ('taskList or newTaskButton not found in DOM');
+        }
+
+        if (buttonElement) {
+            buttonElement.addEventListener('click', () => {
+                //hides save-button and shows new task button
+                newTaskButton.style.display = 'block';
+                buttonElement.style.display = 'none';
+
+            });
+        }
 }
